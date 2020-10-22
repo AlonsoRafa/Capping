30db0577a210430f6fc611f5afa7ce8aa01c71ac
AlonsoRafa <44969258+AlonsoRafa@users.noreply.github.com>
2020-10-23 01:25:09 +0200

    Update esES.lua
    - Spanish (from Spain) version. Might not be suitable for MX.
    - Some translations can be more accurate but have been kept neutral gender-wise due lack of exact context. (easily fixable)

--- Locales/esES.lua
+++ Locales/esES.lua
@@ -3,24 +3,24 @@ if GetLocale() ~= "esES" and GetLocale() ~= "esMX" then return end
 local _, mod = ...
 local L = mod.L
 
---L.battleBegins = "Battle Begins"
---L.finalScore = "Final: %d - %d"
---L.flagRespawns = "Flag respawns"
+L.battleBegins = "Inicio de batalla"
+L.finalScore = "Final: %d - %d"
+L.flagRespawns = "Reaparición de bandera"
 
---L.takenTheFlagTrigger = "^(.+) has taken the flag!"
---L.hasTakenTheTrigger = "has taken the"
---L.upgradeToTrigger = "Upgrade to"
---L.droppedTrigger = "dropped"
---L.capturedTheTrigger = "captured the"
+L.takenTheFlagTrigger = "^(.+) ha cogido la bandera!"
+L.hasTakenTheTrigger = "ha cogido la"
+L.upgradeToTrigger = "Mejorado/a a"
+L.droppedTrigger = "ha soltado la"
+L.capturedTheTrigger = "ha capturado la"
 
---L.hordeGate = "Horde Gate"
---L.allianceGate = "Alliance Gate"
+L.hordeGate = "Puerta de la Horda"
+L.allianceGate = "Puerta de la Alianza"
 --L.gatePosition = "%s (%s)"
---L.west = "West"
+L.west = "Oeste"
 --L.front = "Front"
---L.east = "East"
---L.hordeBoss = "Horde Boss"
---L.allianceBoss = "Alliance Boss"
+L.east = "Este"
+L.hordeBoss = "Jefe Horda"
+L.allianceBoss = "Jefe Alianza"
 --L.galvangar = "Galvangar"
 --L.balinda = "Balinda"
 L.ivus = "Ivus" -- Ivus el Señor del Bosque
@@ -28,31 +28,31 @@ L.lokholar = "Lokholar" -- Lokholar el Señor del Hielo
 --L.handIn = "|cFF33FF99Capping|r: Automatically handing in quest items."
 
 --- Alliance IoC Workshop yells:
--- Gnomish Mechanic yells: I'm halfway there! Keep the Horde away from here.  They don't teach fighting in engineering school!
--- Gnomish Mechanic yells: It's broken already?! No worries. It's nothing I can't fix.
+-- Gnomish Mechanic yells: ¡Voy por la mitad! Mantén a la Horda lejos de aquí. ¡No enseñan a pelear en la escuela de ingeniería!
+-- Gnomish Mechanic yells: ¡¿Ya se ha roto?! No hay problema. Nada que no pueda arreglar.
 --- Horde IoC Workshop yells:
--- Goblin Mechanic yells: I'm about halfway done! Keep the Alliance away - fighting's not in my contract!
--- Goblin Mechanic yells: It's broken again?! I'll fix it... just don't expect the warranty to cover this.
---L.halfway = "halfway"
---L.broken = "broken"
+-- Goblin Mechanic yells: ¡Voy por la mitad! Mantén a la Alianza a raya - ¡luchar no está en mi contrato!
+-- Goblin Mechanic yells: ¡¿Otra vez roto?! Lo arreglaré... pero no esperes que la garantía cubra esto.
+--L.halfway = "al 50%"
+--L.broken = "roto"
 
 -- Wintergrasp
---L.damaged = "|cFF33FF99Capping|r: %s Damaged"
---L.destroyed = "|cFF33FF99Capping|r: %s Destroyed"
---L.northEastKeep = "North-East Fortress Tower"
---L.southEastKeep = "South-East Fortress Tower"
---L.northWestKeep = "North-West Fortress Tower"
---L.southWestKeep = "South-West Fortress Tower"
---L.northWest = "North-West Wall"
---L.southWest = "South-West Wall"
---L.south = "South Wall"
---L.southEast = "South-East Wall"
---L.northEast = "North-East Wall"
---L.innerWest = "Inner-West Wall"
---L.innerSouth = "Inner-South Wall"
---L.innerEast = "Inner-East Wall"
---L.southGate = "South Gate"
---L.mainEntrance = "Main Entrance"
---L.westTower = "West Tower"
---L.southTower = "South Tower"
---L.eastTower = "East Tower"
+L.damaged = "|cFF33FF99Capping|r: %s Dañado/a"
+L.destroyed = "|cFF33FF99Capping|r: %s Destruido/a"
+L.northEastKeep = "Torre de la fortaleza Noreste"
+L.southEastKeep = "Torre de la Fortaleza Sureste"
+L.northWestKeep = "Torre de la Fortaleza Noroeste"
+L.southWestKeep = "Torre de la Fortaleza Suroeste"
+L.northWest = "Muro Noroeste"
+L.southWest = "Muro Suroeste"
+L.south = "Muro Sur"
+L.southEast = "Muro Sureste"
+L.northEast = "Muro Noreste"
+L.innerWest = "Muro interior Oeste"
+L.innerSouth = "Muro interior Sur"
+L.innerEast = "Muro interior Este"
+L.southGate = "Puerta Sur"
+L.mainEntrance = "Entrada principal"
+L.westTower = "Torre Oeste"
+L.southTower = "Torre Sur"
+L.eastTower = "Torre Este"
